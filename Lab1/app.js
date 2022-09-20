ab74a67ba729101a728951352733e0f9b20d392e
KeyOne <trantuanduy.05112001@gmail.com>
2022-09-20 13:53:57 +0700

    update 4

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,19 +1,34 @@
 const express = require("express");
 // Enable cross-origin resource sharing (CORS) with various options.
 const cors = require("cors");
-const { type } = require("os");
+
+// Morgan will log response codes after the response is sent
+const morgan = require("morgan");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 // parse JSON request to JS object
 app.use(express.json());
-let text = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+app.use(morgan('dev'));
+
+// register routers
+app.use("/api/contacts", contactsRouter);
+
 app.get('/', (req, res, next) => {
     res.json({message: "Welcome to contact book application."});
+});
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
